6548c476f6616901ca6c77b0a045f8d9d91ddb2b
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2019-03-10 05:20:12 -0400

    Fixed the chart scaling issue.

--- app/public/js/line-chart.js
+++ app/public/js/line-chart.js
@@ -1,23 +1,26 @@
-new Chart(document.getElementById("line"), {
+$(document).ready(function() {
+  new Chart(document.getElementById("line"), {
     type: 'line',
     data: {
       labels: [01, 05, 10, 15, 20, 25, 30],
       datasets: [{ 
-          data: [86, 114, 106, 106, 107, 111, 133],
-          label: "Green Spendings",
-          borderColor: "#3e95cd",
-          fill: false
-        }, { 
-          data: [282, 350, 411, 502, 635, 809, 947],
-          label: "Non-Green Spendings",
-          borderColor: "#8e5ea2",
-          fill: false
-        }]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'March 2019 Spedings (in USD)'
-        }
+        data: [86, 114, 106, 106, 107, 111, 133],
+        label: "Green Spendings",
+        borderColor: "#3e95cd",
+        fill: false
+      }, { 
+        data: [282, 350, 411, 502, 635, 809, 947],
+        label: "Non-Green Spendings",
+        borderColor: "#8e5ea2",
+        fill: false
+      }]
+    },
+    options: {
+      responsive: false,
+      title: {
+        display: true,
+        text: 'March 2019 Spedings (in USD)'
       }
-    });
+    }
+  });
+});
---
+++ app/public/js/measure.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  const pageWidth = document.getElementById("line").style.width;
+  document.getElementById("bottom").style.width = pageWidth;
+});
