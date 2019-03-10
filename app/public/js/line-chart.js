052e9303fcb4bb2a1f6034d1629508cceb32da36
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2019-03-10 00:26:06 -0500

    My files.

---
+++ app/public/js/line-chart.js
@@ -0,0 +1,23 @@
+new Chart(document.getElementById("line"), {
+    type: 'line',
+    data: {
+      labels: [01, 05, 10, 15, 20, 25, 30],
+      datasets: [{ 
+          data: [86, 114, 106, 106, 107, 111, 133],
+          label: "Green Spendings",
+          borderColor: "#3e95cd",
+          fill: false
+        }, { 
+          data: [282, 350, 411, 502, 635, 809, 947],
+          label: "Non-Green Spendings",
+          borderColor: "#8e5ea2",
+          fill: false
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'March 2019 Spedings (in USD)'
+        }
+      }
+    });
